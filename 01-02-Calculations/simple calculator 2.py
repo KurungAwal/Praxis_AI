315d644ea558b2d74a6facaba547b5d4d82e6c00
KurungAwal <faiz.akbar.abdullah@gmail.com>
2024-07-30 14:52:31 +0700

    Learning calculator

---
+++ 01-02-Calculations/simple calculator 2.py
@@ -0,0 +1,14 @@
+x = input()
+y = input()
+
+add = x + y
+substract = x - y
+Multiple = x * y
+Divide = x/y
+
+print("Choose add, substract, multiple or divide?")
+choice = input("Enter your choice: ")
+
+
+
+
---
+++ 01-02-Calculations/simple calculator.py
@@ -0,0 +1,7 @@
+print("Please Input Your First Number For Multiplication: ")
+x = input()
+print("Multiple? ")
+y = input()
+
+
+print("Result: ", int(x) * int(y))
