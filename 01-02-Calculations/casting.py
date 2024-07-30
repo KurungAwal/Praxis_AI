3bf76fea551e0b8eb1c92dabe28bccdae54cccb7
KurungAwal <faiz.akbar.abdullah@gmail.com>
2024-07-30 11:35:43 +0700

    Learn casting And String

---
+++ 01-02-Calculations/casting.py
@@ -0,0 +1,11 @@
+# x = "3.75"
+# y = float(x)
+
+# print(y)
+
+x = "3.75"
+y = "4.0"
+
+result = (float(x) + float(y))
+
+print(result)
---
+++ 01-02-Calculations/strings.py
@@ -0,0 +1,9 @@
+word = "Matur Nuwun"
+word2 = "Ngantuk"
+p = "ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp"
+
+# if we want to specify the string characters, then
+print(word[-1])
+
+# if we want to know the length of the string, then
+print(len(p))
