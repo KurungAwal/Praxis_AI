639a863de23674a30c30fac50e257185c6303cf2
KurungAwal <faiz.akbar.abdullah@gmail.com>
2024-08-01 20:48:27 +0700

    New update calculator

--- 01-03-Conditionals/multiple_calculator.py
+++ 01-03-Conditionals/multiple_calculator.py
@@ -5,9 +5,10 @@ os.system('cls')
 
 
 # add = "add"
-# substract = "substract"
-# multiple = "multiple"
-# divide = "divide"
+    # substract = "substract"
+    # multiple = "multiple"
+    # divide = "divide"
+
 quit = "quit"
 
 math = ("add", "substract", "multiple", "divide")
@@ -21,74 +22,79 @@ buffer = ""
 buffer2 = ""
 
 
-print("Welcome new user!")
+def calculator():
+           while True:    
 
+                print("----------------------------------------------------------------")
+
+                input_calculate = input("What do you want? Add, substract, multiple, divide or quit? ")
+
+                while True:
+                        buffer = input("Input your first number: ")
+                        try:
+                            input1 = int(buffer)
+                            break
+                        except:
+                            print("Invalid input")
+
+                while True:
+                        buffer2 = input("Input your second number: ")
+                        try:
+                            input2 = int(buffer2)
+                            break
+                        except:
+                            print("Invalid input")
+
+                if input_calculate == w:
+                    result = print("Result: ", int(input1) + int(input2))
+                        
+                elif input_calculate == x:
+                    result = print("Result: ", int(input1) - int(input2))
+                    #yesorno = input("Want to calculate again? yes/no")
+
+                elif input_calculate == y:
+                    result = print("Result: ", int(input1) * int(input2))
+                    
+                elif input_calculate == z:
+                    result = print("Result: ", int(input1) / int(input2))
+
+                elif input_calculate == quit:
+                    break
+
+                else:
+                    print("Invalid input") 
+
+
+print("Welcome new user!")
 
 
 user = input("Input your new username: ") #Pada website, variabel akan diambil dari database website itu sendiri
 password = getpass.getpass("Input you new password: ")
-     
+        
 
 print("----------------------------------------------------------------")
 
-print("Please Login")
-user_in = input("Input the username: ")
-pw_in = getpass.getpass("Input your password: ")
 
-if user_in == user and password == pw_in:
-    print("----------------------------------------------------------------")
-    print("Welcome to this calculator " + user)
+while True:
+      
+    print("Please Login")
+    user_in = input("Input the username: ")
+    pw_in = getpass.getpass("Input your password: ")
 
-
-else:
-    print("----------------------------------------------------------------")
-    print("Access denied")
-    
-     
-while True:    
-
-            print("----------------------------------------------------------------")
-
-            input_calculate = input("What do you want? Add, substract, multiple, divide or quit? ")
-
-            while True:
-                    buffer = input("Input your first number: ")
-                    try:
-                        input1 = int(buffer)
-                        break
-                    except:
-                        print("Invalid input")
-
-            while True:
-                    buffer2 = input("Input your second number: ")
-                    try:
-                         input2 = int(buffer2)
-                         break
-                    except:
-                         print("Invalid input")
-
-            if input_calculate == w:
-                result = print("Result: ", int(input1) + int(input2))
-                    
-            elif input_calculate == x:
-                result = print("Result: ", int(input1) - int(input2))
-                #yesorno = input("Want to calculate again? yes/no")
-
-            elif input_calculate == y:
-                result = print("Result: ", int(input1) * int(input2))
-                
-            elif input_calculate == z:
-                result = print("Result: ", int(input1) / int(input2))
-
-            elif input_calculate == quit:
-                break
-
-            else:
-                print("Invalid input")
+    if user_in == user and password == pw_in:
             
+            print("----------------------------------------------------------------")
+            print("Welcome to this calculator " + user)
+            calculator()
+
+
+    else:
+            print("----------------------------------------------------------------")
+            print("Access denied")
+            print("----------------------------------------------------------------")
+        
 
 
 
-
-
+                
 
---
+++ 01-04-Functions/functions.py
@@ -0,0 +1,14 @@
+
+
+def addition(number1, number2):
+    return number1+number2
+
+def welcome():
+    print("Selamat Datang")
+    print("Welcome")
+    print("Halo")
+
+result = addition(3, 5)
+print(result)
+
+welcome()
